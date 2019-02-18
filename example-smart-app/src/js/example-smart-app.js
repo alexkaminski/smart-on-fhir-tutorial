bb033ce0b5acd910eaf6b09a45c92b363e1261f9
Alex Kaminski <akaminski@myvps.org>
2019-02-18 11:35:21 -0800

    added zip code

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,7 +8,7 @@
     }
 
     function onReady(smart)  {
-      console.log(smart);
+      
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
@@ -38,6 +38,7 @@
           }
 
           var height = byCodes('8302-2');
+          var zipcode = byCodes('45401-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -49,6 +50,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          p.zipcode = zipcode;
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -84,6 +86,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      zipcode: {value: ''}
     };
   }
 
@@ -127,6 +130,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#zipcode').html(p.zipcode);
   };
 
 })(window);
